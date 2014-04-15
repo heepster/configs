1ea138098e8dc176721b7af4cefca4ab0752c484
Kevin Huynh <kevin@factual.com>
2014-04-15 10:44:47 -0700

    Adding rails grep to fish

--- fish/config.fish
+++ fish/config.fish
@@ -14,5 +14,6 @@ alias postgres 'postgres -D /usr/local/var/postgres'
 alias mastermind 'lein run --port 3003 -r'
 alias fish 'chsh -s /usr/local/bin/fish'
 alias configs "cd $PROJECT_DIR/configs"
+alias railsgrep "grep --exclude-dir=log --exclude-dir=test -ri"
 
 
