14a8465d127f9164152f0fab0f9f841a42361c4b
heepster <kevinhuynh1@gmail.com>
2016-12-01 14:52:45 -0800

    Latest updates

--- fish/config.fish
+++ fish/config.fish
@@ -1,24 +1,19 @@
 ### ENVIRONMENT VARIABLES
-
 set -U PROJECTS_DIR  "$HOME/code/projects"
 set -U WORKSPACE_DIR "$HOME/code/workspace"
 set -U CONFIGS_DIR   "$PROJECTS_DIR/configs"
 set -U BIN_DIR       "$HOME/code/bin"
+set -U WORK_DESKTOP_IP "10.0.201.41"
 
 ### ABBREVIATIONS
 
 ## General
-
 abbr -a s "source ~/.config/fish/config.fish"
 abbr -a f "cd $PROJECTS_DIR"
 abbr -a w "cd $WORKSPACE_DIR"
 abbr -a b "cd $BIN_DIR"
 abbr -a sc 'echo "Saving configuration files"; and cd $CONFIGS_DIR; and git add .; and git commit -m "Latest updates"; and git push'
-
-## MAVEN
-
-abbr -a mcc 'mvn clean compile'
-abbr -a mej 'mvn exec:java'
+abbr -a wd "ssh kevin@$WORK_DESKTOP_IP"
 
 ## Java
 set -g -x JAVA_8_HOME (/usr/libexec/java_home -v1.8)
@@ -28,19 +23,17 @@ abbr -a java8 'set -g -x JAVA_HOME $JAVA_8_HOME'
 set -g -x JAVA_HOME $JAVA_8_HOME
 
 ## TMUX
-
 abbr -a tl 'tmux list-sessions'
 abbr -a ta 'tmux attach -t '
 
 ## GIT
-
 abbr -a ga "git add ."
 abbr -a gd "git diff HEAD *"
 abbr -a gc "git commit -m"
 abbr -a gs "git status"
-abbr -a gr "git reset HEAD "
 
 ## NVM
+#eval $NVM_CMD use 4.4.5
 set -l BASS_PREFIX "bass source ~/.nvm/nvm.sh --no-use ';'"
 set -l NVM_CMD "$BASS_PREFIX nvm"
 set -l NPM_CMD "$BASS_PREFIX npm"
@@ -55,12 +48,3 @@ abbr -a de "set ID (docker ps | grep -v CONTAINER | awk '{print \$1}' | head -n 
 
 # Kill most recent docker container
 abbr -a dk "set ID (docker ps | grep -v CONTAINER | awk '{print \$1}' | head -n 1); docker kill \$ID"
-rvm default
-
-### ENVIRONMENT SETUP 
-
-## NVM
-eval $NVM_CMD use 4.4.5
-
-## PATH
-set -gx PATH /usr/local/Cellar/vim/7.4.2334/bin/ (eval $NPM_CMD bin -g) $PATH
