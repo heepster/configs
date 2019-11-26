66679319cb67ab76ca110018a190c6d1f98b7e4a
Kevin Huynh <kevinhuynh1@gmail.com>
2019-11-25 19:01:34 -0800

    updates

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -12,7 +12,7 @@ module.exports = {
     fontSize: 13,
 
     // font family with optional fallbacks
-    fontFamily: 'Monaco, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'Monaco, Roboto Mono, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
