4456cceeaebc5203371c10db4712c2a38ab4b21a
Kevin Huynh <kevinhuynh1@gmail.com>
2020-04-09 14:21:57 -0700

    Latest

--- fish/config.fish
+++ fish/config.fish
@@ -11,16 +11,6 @@ abbr -a c "cd $CONFIGS_DIR"
 abbr -a sc 'echo "Saving configuration files"; and cd $CONFIGS_DIR; and git add .; and git commit -m "Latest updates"; and git push'
 abbr -a s "source ~/.config/fish/config.fish"
 
-## Java
-#switch (uname)
-#case Darwin
-#  set -g -x JAVA_8_HOME (/usr/libexec/java_home -v1.8)
-#  abbr -a java8 'set -g -x JAVA_HOME $JAVA_8_HOME'
-#  set -g -x JAVA_HOME $JAVA_8_HOME
-#case Linux 
-#  # Linux has good support for Java version switching already
-#end
-
 ## TMUX
 abbr -a tl 'tmux list-sessions'
 abbr -a ta 'tmux attach -t '
@@ -37,10 +27,13 @@ abbr -a vim "nvim"
 abbr -a vc "nvim ~/.config/nvim/init.vim"
 abbr -a fc "nvim ~/.config/fish/config.fish"
 
-abbr -a node "nvm; and node"
-
 switch (uname)
 case Darwin
+  # Set Path
   set -gx PATH ~/Library/Python/3.7/bin /usr/local/opt/python/libexec/bin /usr/local/bin ~/.cargo/bin $PATH
+
+  # Workaround for slow auto completion
+  function __fish_describe_command; end
 case Linux
 end
+
